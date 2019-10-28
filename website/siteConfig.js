a62d49bf5fd5cbf6e3ec8249835a84fed3b42972
hyochan <dooboolab@gmail.com>
2019-10-28 10:04:24 +0900

    Update siteConfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,12 @@ const users = [
 const siteConfig = {
   title: 'HackaTalk', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+  url: 'https://dooboolab.github.io', // Your website URL
+  baseUrl: '/hackatalk/', // Base URL for your project */
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'hackatalk-website',
+  organizationName: 'dooboolab',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
